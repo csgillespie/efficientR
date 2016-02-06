480ed093c27d200eb65b68412a498fd12b9f64d8
Colin Gillespie <csgillespie@gmail.com>
2016-02-06 12:20:13 +0000

    Adding first draft of chapter 6

---
+++ R/initialise.R
@@ -0,0 +1,9 @@
+alpha=255
+colours = c(rgb(200,79,178, alpha=alpha,maxColorValue=255), 
+            rgb(105,147,45, alpha=alpha, maxColorValue=255),
+            rgb(85,130,169, alpha=alpha, maxColorValue=255),
+            rgb(204,74,83, alpha=alpha, maxColorValue=255),
+            rgb(183,110,39, alpha=alpha, maxColorValue=255),
+            rgb(131,108,192, alpha=alpha, maxColorValue=255),
+            rgb(63,142,96, alpha=alpha, maxColorValue=255))
+palette(colours)
