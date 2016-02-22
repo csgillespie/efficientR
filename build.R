cd3b6cb24efaa1040b349f1ce23b378feb37cec2
Colin Gillespie <csgillespie@gmail.com>
2016-02-22 21:27:02 +0000

    Fixing web version of the book. Switching to gitbook

---
+++ build.R
@@ -0,0 +1,9 @@
+
+l = list("download"="pdf", 
+  "edit"=list("link"="https://github.com/csgillespie/efficientR/edit/master/%s",
+              "text"="Edit")); 
+bookdown::render_book("index.Rmd", 
+                      output_format = bookdown::gitbook(config=l))
+
+system("cp style.css _book")
+
