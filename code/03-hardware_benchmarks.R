8295f1cdd4bf7fd686e05e8f6ba680cd755311fe
Colin Gillespie <csgillespie@gmail.com>
2016-04-27 11:03:25 +0100

    Tweak graphics and unnumber exercises

---
+++ code/03-hardware_benchmarks.R
@@ -0,0 +1,33 @@
+library("benchmarkme")
+library(ggplot2)
+# results = plot_past()
+# times = sort(tapply(results$time, results$id, mean))
+# times = times/min(times)
+# past_results = data.frame(times, rank=1:length(times))
+#save(past_results, file="data/past_results.RData")
+load("data/past_results.RData")
+
+par(mar=c(3,3,2,1), mgp=c(2,0.4,0), tck=-.01,
+    cex.axis=0.9, las=1, xaxs='i',yaxs='i')
+plot(0.1, type="n", ylim=c(0.95, 100), xlim=c(0, 158), axes=FALSE, frame=FALSE, 
+     xlab="Rank", ylab="Relative Time", 
+     log="y")
+points(past_results$rank, past_results$times, 
+       pch=21, bg=get_col(3, 240), 
+       col="grey90", cex=0.9)
+
+
+abline(h=c(1, 2, 5, 10, 20, 50, 100), lty=3, col="grey80")
+
+#axis(2)
+axis(1, tick = FALSE)
+axis(2,c(1, 2,10, 20,100), c(1, 2,10, 20,100),tick = FALSE)
+
+text(130, 50, "Intel Atom @ 1.66GHz", pos=3, col="grey20", cex=0.9)
+
+
+title("CPU Benchmarks", adj=1, 
+      cex.main=0.9, font.main=2, col.main="black")
+
+
+
--- code/initialise.R
+++ code/initialise.R
@@ -7,3 +7,13 @@ colours = c(rgb(200,79,178, alpha=alpha,maxColorValue=255),
             rgb(131,108,192, alpha=alpha, maxColorValue=255),
             rgb(63,142,96, alpha=alpha, maxColorValue=255))
 palette(colours)
+
+get_col = function(i, alpha=255) {
+  c(rgb(200,79,178, alpha=alpha,maxColorValue=255), 
+    rgb(105,147,45, alpha=alpha, maxColorValue=255),
+    rgb(85,130,169, alpha=alpha, maxColorValue=255),
+    rgb(204,74,83, alpha=alpha, maxColorValue=255),
+    rgb(183,110,39, alpha=alpha, maxColorValue=255),
+    rgb(131,108,192, alpha=alpha, maxColorValue=255),
+    rgb(63,142,96, alpha=alpha, maxColorValue=255))[i]
+}
