6adf7d599647f9d996f8761b346a2701a62840a1
Colin Gillespie <csgillespie@gmail.com>
2016-04-28 19:16:25 +0100

    Adding in Rcpp to performance chapter

---
+++ src/mean_c.cpp
@@ -0,0 +1,30 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+double mean_c(NumericVector x){
+  int i;
+  int n = x.size();
+  double mean = 0;
+  
+  for(i=0; i<n; i++){
+    mean = mean + x[i]/n;
+  }
+  return mean;
+}
+
+// [[Rcpp::export]]
+NumericVector res_c(NumericVector x, NumericVector y){
+  int i;
+  int n = x.size();
+  NumericVector residuals(n);
+  for(i=0; i<n; i++){
+    residuals[i] = pow(x[i] - y[i], 2);
+  }
+  return residuals;
+}
+
+// [[Rcpp::export]]
+NumericVector res_sugar(NumericVector x, NumericVector y){
+  return pow(x-y, 2);
+}
