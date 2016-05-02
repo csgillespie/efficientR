f731b80b4b3935e9118bc225969ded01c9ec12b0
Colin Gillespie <csgillespie@gmail.com>
2016-05-02 11:45:16 +0100

    Minor plot tweaks

--- code/02-blas.R
+++ code/02-blas.R
@@ -2,24 +2,25 @@ source("code/initialise.R")
 library("ggplot2")
 res_blas = readRDS("data/res_rl_blas.Rds")
 res_noblas = readRDS("data/res_rl_noblas.Rds")
-res_blas$blas = "BLAS"
-res_noblas$blas = "No BLAS"
+res_blas$blas = "Optimised"
+res_noblas$blas = "Standard"
 res_all = rbind(res_blas, res_noblas)
 
-head(res_all)
-res_all$blas[res_all$blas == "BLAS"] = "Optimised"
-res_all$blas[res_all$blas == "No BLAS"] = "Standard"
+g = ggplot(res_all) + geom_violin(aes(test_group, elapsed, fill = blas),position=position_dodge(0.9))
 
-g = ggplot(res_all) + geom_violin(aes(test_group, elapsed, fill = blas)) 
 
 g1 = g + labs(title = "Performance gains with BLAS", 
-          x = "Benchmark", y = "Elapsed Time") + 
-  theme(panel.grid.major = element_line(colour = "gray90"), 
+          x = "Benchmark", y = "Elapsed Time",colour = NULL, fill = NULL) + 
+  theme(panel.grid.major.y = element_line(colour = "gray90"), 
     panel.grid.minor = element_line(colour = NA), 
+    panel.grid.major.x = element_line(colour = NA), 
     plot.title = element_text(size = 12, 
         face = "bold", hjust = 1, vjust = 0), 
     panel.background = element_rect(fill = NA), 
     legend.background = element_rect(fill = NA), 
-    legend.position = c(0.95, 0.9)) +labs(colour = NULL, fill = NULL) + 
+    legend.position = c(0.93, 0.92), 
+    axis.ticks.x = element_line(linetype = "blank")) + 
   scale_fill_manual(values=c(get_col(2), get_col(3)))
+
+g1 = g1 + scale_y_continuous(limits=c(0,12), expand = c(0, 0))
 print(g1)
