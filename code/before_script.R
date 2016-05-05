53afffbca96c596dba142f34d4f1cfa4d89344e8
Colin Gillespie <csgillespie@gmail.com>
2016-05-05 16:19:56 +0100

    Move knitr options into before_script. Standardise the number of digits and how much dplyr displays

--- code/before_script.R
+++ code/before_script.R
@@ -3,3 +3,13 @@ library("methods")
 library("microbenchmark")
 ## Needed because CSG doesn't load datasets
 data("USArrests", package="datasets")
+
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE
+)
+set.seed(2016)
+options(digits = 3)
+options(dplyr.print_min = 5, dplyr.print_max = 5)
