479373d9bac92ac0550690f6a57e86b83c42bcc2
robinlovelace <rob00x@gmail.com>
2016-06-20 23:52:05 +0100

    Add script to rename chapters

--- code/docstats.R
+++ code/docstats.R
@@ -15,3 +15,21 @@ for(i in 1:length(f)){
   s = stri_stats_latex(d)
   doc_stats$Words[i] = s["Words"]
 }
+
+doc_stats
+
+# # code to rename chapters (run once)
+# oldnums = 6:10
+# newnums = oldnums + 1
+# oldnums[nchar(oldnums) == 1] = paste0("0", oldnums[nchar(oldnums) == 1])
+# newnums[nchar(newnums) == 1] = paste0("0", newnums[nchar(newnums) == 1])
+# i = 1
+# fnew = f
+# for(i in seq_along(oldnums)){
+#   j = grep(pattern = oldnums[i], f)
+#   fnew[j] = gsub(pattern = oldnums[i], replacement = newnums[i], x = f[j])
+#   file.rename(f[j], fnew[j])
+# }
+# 
+# fnew
+# 
