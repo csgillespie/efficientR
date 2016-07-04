916344670dea70def1c36186a80ca34a00dca469
Colin Gillespie <csgillespie@gmail.com>
2016-07-04 14:59:29 +0100

    Fix latex figure widths

--- code/before_script.R
+++ code/before_script.R
@@ -8,8 +8,10 @@ data("USArrests", package="datasets")
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  cache = TRUE
+  cache = TRUE, 
+  fig.align="center",
+  fig.pos="t"
 )
 set.seed(2016)
 options(digits = 3)
-options(dplyr.print_min = 5, dplyr.print_max = 5)
+options(dplyr.print_min = 4, dplyr.print_max = 4)
