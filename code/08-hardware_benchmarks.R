0f6ec33337c71baa6e80233d3fb535a38128ac02
Colin Gillespie <csgillespie@gmail.com>
2016-11-02 13:38:07 +0000

    Lower case axis label

--- code/08-hardware_benchmarks.R
+++ code/08-hardware_benchmarks.R
@@ -10,7 +10,7 @@ source("code/initialise.R")
 par(mar=c(3,3,2,1), mgp=c(2,0.4,0), tck=-.01,
     cex.axis=0.9, las=1, xaxs='i',yaxs='i')
 plot(0.1, type="n", ylim=c(0.95, 100), xlim=c(0, 158), axes=FALSE, frame=FALSE, 
-     xlab="Rank", ylab="Relative Time", 
+     xlab="Rank", ylab="Relative time", 
      log="y", panel.first = abline(h=c(1, 2, 5, 10, 20, 50, 100), lty=1, col="grey90"))
 points(past_results$rank, past_results$times, 
        pch=21, bg=get_col(3, 240), 
@@ -19,7 +19,7 @@ points(past_results$rank, past_results$times,
 
 ## Axis
 axis(1, tick = FALSE,  col.axis="grey50", cex.axis = 0.8)
-axis(2,c(1, 2,10, 20,100), c(1, 2,10, 20,100), tick = FALSE,  col.axis="grey50", cex.axis = 0.8)
+axis(2,c(1, 2,5,10, 20,50, 100), c(1, 2,5,10, 20,50, 100), tick = FALSE,  col.axis="grey50", cex.axis = 0.8)
 
 ## Point out the slow CPU
 text(130, 50, "Intel Atom @ 1.66GHz", pos=3, cex=0.9)
